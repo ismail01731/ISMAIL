ee74f7c4f29f566e3072ca736375e649bccbff79
Ismail Khan <imd483704@gmail.com>
2026-09-10 07:49:54 +0600

    Fix ProjectFiles JavaScript error

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 ﻿const chatContainer = document.getElementById("chatContainer");
-const messageInput = document.getElementById("messageInput");ProjectFiles.txt
+const messageInput = document.getElementById("messageInput");
 const sendButton = document.getElementById("sendButton");
 const attachButton =
     document.getElementById("attachButton");
